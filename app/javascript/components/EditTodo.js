ffaa3187b6638e416e2455e04db323c114b50f29
tdiccccc <99638807+tdiccccc@users.noreply.github.com>
2022-07-05 23:54:52 +0900

    [Add]edittodo

--- app/javascript/components/EditTodo.js
+++ app/javascript/components/EditTodo.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React,  {useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+//import { toast } from 'react-toastify'
+//import 'react-toastify/dist/ReactToastify.css'
+
+const InputName = styled.input`
+  font-size: 20px;
+  width: 100%;
+  height: 40px;
+  padding: 2px 7px;
+  margin: 12px 0;
+`
+
+const CurrentStatus = styled.div`
+  font-size: 19px;
+  margin: 8px 0 12px 0;
+  font-weight: bold;
+`
+
+const IsCompeletedButton = styled.button`
+  color: #fff;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  background: #f2a115;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
+
+const EditButton = styled.button`
+  color: white;
+  font-weight: 500;
+  font-size: 17px;
+  padding: 5px 10px;
+  margin: 0 10px;
+  background: #0ac620;
+  border-radius: 3px;
+  border: none;
+`
+
+const DeleteButton = styled.button`
+  color: #fff;
+  font-size: 17px;
+  font-weight: 500;
+  padding: 5px 10px;
+  background: #f54242;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+`
 
 function EditTodo() {
+
+  const initialTodoState = {
+    id: null,
+    name: "",
+    is_completed: false
+  }
+  
   return (
-    <div>EditTodo</div>
+    <>
+      <h1>Editing Todo</h1>
+    </>
   )
 }
 
